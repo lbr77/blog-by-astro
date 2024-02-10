27452dbb071062c036cd158256f70835d1e9bd00
Sakura_Li <me@nvme0n1p.dev>
2024-02-10 15:03:11 +0800

    service worker不能要了（悲

--- src/pages/action/purge.ts
+++ src/pages/action/purge.ts
@@ -2,5 +2,5 @@
 const uuid = "6c23781e-9df8-44ec-92b4-baf4ccced087";
 // GET  /action/purge?pageId=
 export async function GET(ctx){
-
+    return new Response("Not implemented", { status: 501 });
 }
