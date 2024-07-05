c3b08e885d7cc8bdc5a093b787817e3729278484
LiBr <me@nvme0n1p.dev>
2024-07-05 09:54:19 +0800

    chore: Update Dockerfile for containerized deployment

--- src/server_env.ts
+++ src/server_env.ts
@@ -10,4 +10,4 @@ export const NUMBER_OF_POSTS_PER_PAGE = process.env.NUMBER_OF_POSTS_PER_PAGE || 
 export const DATABASE_ID=NOTION_LINK_DATABASE_ID;
 export const BASE_PATH = process.env.BASE_URL || "";
 export const REDIS_URL = process.env.REDIS_URL || "";
-export const ENABLE_REDIS = process.env.ENABLE_REDIS || "false";
+export const ENABLE_REDIS = process.env.ENABLE_REDIS || false;
